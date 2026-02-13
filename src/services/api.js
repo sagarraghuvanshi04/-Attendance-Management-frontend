37854bc9503b1c2a8e6b01c185e954855a15cb16
DeepakGupta4 <dkguptak9369.com>
2026-02-13 13:06:37 +0530

    Update backend URL to new Render service

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://university-management-api.onrender.com/api",
+  baseURL: "https://librarymanagementbackend-ztsr.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
