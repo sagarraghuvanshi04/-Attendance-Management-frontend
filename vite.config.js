02b762cdc1917cfae892f77380361f9c36e7de50
DeepakGupta4 <dkguptak9369.com>
2026-02-13 12:40:39 +0530

    fixing problem app is not publishing on vercel

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vendor': ['react', 'react-dom', 'react-router-dom'],
-          'ui': ['lucide-react', 'react-hot-toast'],
-        }
-      }
-    },
     minify: 'terser',
     terserOptions: {
       compress: {
